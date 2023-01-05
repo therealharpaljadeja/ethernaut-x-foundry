95a26e155edb58754ef508348f650681f8d94ade
therealharpaljadeja <jadejaharpal14@gmail.com>
2023-01-05 20:18:01 +0530

    feat: modify `Instance.t.sol` to emulate Ethernaut

--- test/Instance.t.sol
+++ test/Instance.t.sol
@@ -2,20 +2,35 @@
 pragma solidity ^0.8.0;
 
 import "ds-test/test.sol";
-import "../src/HelloEthernaut.sol";
+import "../src/levels/Instance/Instance.sol";
+import "../src/levels/Instance/InstanceFactory.sol";
+import "../src/Ethernaut.sol";
+import {Vm} from "forge-std/Vm.sol";
 
-contract HelloEthernautTest is DSTest {
-    HelloEthernaut public helloEthernaut;
+contract InstanceTest is DSTest {
+    Vm private constant vm = Vm(HEVM_ADDRESS);
+    Ethernaut ethernaut;
+    Instance instance;
+    address levelAddress;
+    InstanceFactory instanceFactory;
 
     function setUp() public {
         // Initializing contract with password - "ethernaut0" is the password used by Ethernaut.
-        helloEthernaut = new HelloEthernaut("ethernaut0");
+        ethernaut = new Ethernaut();
+        instanceFactory = new InstanceFactory();
+        ethernaut.registerLevel(instanceFactory);
+
+        assertTrue(ethernaut.registeredLevels(address(instanceFactory)));
     }
 
-    function testIsHelloEthernautCleared() public {
+    function testInstanceCleared() public {
+        vm.startPrank(address(69));
+        levelAddress = ethernaut.createLevelInstance(instanceFactory);
+        instance = Instance(levelAddress);
         // Skipping through all the functions and directly calling authenticate with the password.
-        helloEthernaut.authenticate(helloEthernaut.password());
+        instance.authenticate(instance.password());
         // Checking if level is cleared!
-        assertTrue(helloEthernaut.getCleared());
+        assertTrue(ethernaut.submitLevelInstance(payable(levelAddress)));
+        vm.stopPrank();
     }
 }
