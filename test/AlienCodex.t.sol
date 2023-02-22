a33def7f21d96807a88d66bc82d0dc0e8253dc90
Harpal Jadeja <jadejaharpal14@gmail.com>
2023-02-22 13:29:33 +0530

    refactor: AlienCodex

--- test/AlienCodex.t.sol
+++ test/AlienCodex.t.sol
@@ -1,19 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "ds-test/test.sol";
-import "forge-std/vm.sol";
-import "../src/Ethernaut.sol";
-import "../src/BaseLevel.sol";
+import "./BaseTest.sol";
 
-contract AlienCodexTest is DSTest {
-    Ethernaut ethernaut;
+contract AlienCodexTest is BaseTest {
     address alienCodexFactory;
-    address eoa = address(69);
-    Vm private constant vm = Vm(HEVM_ADDRESS);
 
     function setUp() public {
-        ethernaut = new Ethernaut();
         address alienCodexFactoryMem;
 
         bytes memory factoryBytecode = abi.encodePacked(
@@ -30,16 +23,13 @@ contract AlienCodexTest is DSTest {
 
         alienCodexFactory = alienCodexFactoryMem;
 
-        ethernaut.registerLevel(Level(alienCodexFactory));
+        super.setUp(Level(alienCodexFactory));
     }
 
-    function testIsAlienCodexCleared() public {
-        vm.startPrank(eoa);
-
-        address instance = ethernaut.createLevelInstance(
-            Level(alienCodexFactory)
-        );
-
+    function testIsAlienCodexCleared()
+        public
+        testWrapper(Level(alienCodexFactory))
+    {
         (bool makeContactSuccess, ) = instance.call(
             abi.encodeWithSignature("make_contact()")
         );
@@ -62,9 +52,5 @@ contract AlienCodexTest is DSTest {
             )
         );
         require(reviseSuccess, "revise() failed");
-
-        assertTrue(ethernaut.submitLevelInstance(payable(instance)));
-
-        vm.stopPrank();
     }
 }
